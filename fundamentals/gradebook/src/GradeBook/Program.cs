31f1b0f6cc910893ea87554559fbf2d3ecc79f14
Alina <lindenalina.al@gmail.com>
2020-09-30 17:10:29 +0100

    var keyword

--- fundamentals/gradebook/src/GradeBook/Program.cs
+++ fundamentals/gradebook/src/GradeBook/Program.cs
@@ -6,10 +6,16 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {   
+            var x = 34.1;
+            var y = 56.8;
+            var result = x + y;
+
+            Console.WriteLine(result);
+
             if (args.Length > 0) {
                 Console.WriteLine($"Hello, {args[0]}!");
             } else {
-                Console.WriteLine($"Hello, World!");
+                Console.WriteLine($"Hello");
             }
             
         }
