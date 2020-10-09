bda7d2541945a165edea6e9db2e047bfd22f2fa2
Alina <lindenalina.al@gmail.com>
2020-10-09 15:24:29 +0100

    throw exceptions

--- fundamentals/gradebook/src/GradeBook/Book.cs
+++ fundamentals/gradebook/src/GradeBook/Book.cs
@@ -34,7 +34,7 @@ namespace GradeBook {
                 grades.Add(grade);
             }
             else {
-                Console.WriteLine("Invalid Value");
+                throw new ArgumentException($"Invalid {nameof(grade)}");
             }
         }
 
